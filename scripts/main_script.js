8f52c09c27f5e8246bf7e0413de5beb038182679
fjordimm <fjordimm@gmail.com>
2024-01-20 15:18:39 -0800

    made progress on ui_control

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -2,6 +2,8 @@
 function mainFunc()
 {
 	const heho = createNewNewsite("eeeeeee");
+	addBiasLineToNewsite(heho, "images/bias_left.png", "Created Bias Text");
+
 	document.getElementById("the-list-of-newsites").appendChild(heho);
 }
 
