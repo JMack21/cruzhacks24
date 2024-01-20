c06ac0d4f7d3eb335569333f1599718ba36433d8
fjordimm <fjordimm@gmail.com>
2024-01-20 14:48:42 -0800

    added node.js and started on the mainFunc and ui_control

---
+++ scripts/main_script.js
@@ -0,0 +1,8 @@
+
+function mainFunc()
+{
+	const heho = createNewNewsite("eeeeeee");
+	document.getElementById("the-list-of-newsites").appendChild(heho);
+}
+
+mainFunc();
---
+++ scripts/ui_control.js
@@ -0,0 +1,12 @@
+
+function createNewNewsite(url)
+{
+	const site = document.createElement("div");
+	site.classList.add("newsite-div");
+
+	const schming = document.createElement("p");
+	schming.innerHTML = "testtintgg";
+	site.appendChild(schming);
+
+	return site;
+}
