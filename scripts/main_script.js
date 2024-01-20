70b399be6ceefdb3b8d04ac41679539831317926
fjordimm <fjordimm@gmail.com>
2024-01-20 15:52:59 -0800

    formatted the ui to work well with many news site and articles

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,13 +1,56 @@
 
 function mainFunc()
 {
-	const heho = createNewNewsite("Haha heho number uno");
-	addBiasLineToNewsite(heho, "images/bias_left.png", "Created Bias Text");
-	addArticleToNewsite(heho, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	const a = createNewNewsite("Site A");
+	addBiasLineToNewsite(a, "images/bias_left.png", "Left Leaning");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(a, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
 
-	const heho2 = createNewNewsite("Heho2");
-	addBiasLineToNewsite(heho2, "images/bias_left.png", "Created Bias Text");
-	addArticleToNewsite(heho2, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	const b = createNewNewsite("Site B");
+	addBiasLineToNewsite(b, "images/bias_left.png", "Right Leaning");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(b, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+
+	const c = createNewNewsite("Site C");
+	addBiasLineToNewsite(c, "images/bias_left.png", "Right Leaning");
+	addBiasLineToNewsite(c, "images/bias_left.png", "Location or something");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(c, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+
+	const d = createNewNewsite("Site D Haha I Have A Long Title");
+	addBiasLineToNewsite(d, "images/bias_left.png", "Right Leaning");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(d, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+
+	const e = createNewNewsite("Site E");
+	addBiasLineToNewsite(e, "images/bias_left.png", "Right Leaning");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
+	addArticleToNewsite(e, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
 }
 
 mainFunc();
