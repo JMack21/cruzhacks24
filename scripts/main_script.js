8720776376582c4bab044f167799f659e024be2f
fjordimm <fjordimm@gmail.com>
2024-01-20 15:37:33 -0800

    ui_control.js is fully functional, so news sites can be added from code

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,10 +1,13 @@
 
 function mainFunc()
 {
-	const heho = createNewNewsite("eeeeeee");
+	const heho = createNewNewsite("Haha heho number uno");
 	addBiasLineToNewsite(heho, "images/bias_left.png", "Created Bias Text");
+	addArticleToNewsite(heho, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
 
-	document.getElementById("the-list-of-newsites").appendChild(heho);
+	const heho2 = createNewNewsite("Heho2");
+	addBiasLineToNewsite(heho2, "images/bias_left.png", "Created Bias Text");
+	addArticleToNewsite(heho2, "https://jrgraphix.net/", "JRGraphix Thing", "Jan 1, 2000");
 }
 
 mainFunc();
