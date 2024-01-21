b357486b5d1d2081e88f4ce1fcaf02182f1c0b2d
fjordimm <fjordimm@gmail.com>
2024-01-21 02:59:35 -0800

    almost got article loading working

--- browserifybundles/bundle.js
+++ browserifybundles/bundle.js
@@ -21,12 +21,44 @@ async function onGottenPageUrl(theUrl)
 {
 	const subjArticleTitle = await articlestuff.getArticleTitle(theUrl);
 
-	const a = uistuff.createNewNewsite("ExampleSiteA");
-	uistuff.addBiasLineToNewsite(a, "images/bias_far_left.png", "Far Left Leaning");
-	uistuff.addArticleToNewsite(a, "https://jrgraphix.net/", "TestArticle", "Jan 1, 2000");
+	const farLeft = uistuff.createNewNewsite("FarLeftSite");
+	uistuff.addBiasLineToNewsite(farLeft, "images/bias_far_left.png", "Far Left Leaning");
 
-	let searchResults = await googlesearch.getGoogleSearchResults('bbc.com', 'obama');
-	console.log(searchResults);
+	const slightLeft = uistuff.createNewNewsite("SlightLeftSite");
+	uistuff.addBiasLineToNewsite(slightLeft, "images/bias_slight_left.png", "Slight Left Leaning");
+
+	const centrist = uistuff.createNewNewsite("Big British Company");
+	uistuff.addBiasLineToNewsite(centrist, "images/bias_centrist.png", "Mainly Centrist");
+
+	const slightRight = uistuff.createNewNewsite("SlightRightSite");
+	uistuff.addBiasLineToNewsite(slightRight, "images/bias_slight_right.png", "Slight Right Leaning");
+
+	const farRight = uistuff.createNewNewsite("FarRightSite");
+	uistuff.addBiasLineToNewsite(farRight, "images/bias_far_right.png", "Far Right Leaning");
+
+	let sites = [[farLeft, 'bbc.com'], [slightLeft, 'bbc.com'], [centrist, 'bbc.com'], [slightRight, 'bbc.com'], [farRight, 'bbc.com']];
+
+	for (let i = 0; i < sites.length; i++)
+	{
+		const siteElm = sites[i][0];
+		const siteStr = sites[i][1];
+
+		console.log('SHe: ' + siteStr);
+
+		let searchResults = await googlesearch.getGoogleSearchResults(siteStr, 'obama');
+		console.log(searchResults);
+		for (let j = 0; j < searchResults.length; j++)
+		{
+			uistuff.addArticleToNewsite(siteElm, searchResults[j], await articlestuff.getArticleTitle(searchResults[j]), 'Jan 0, 0000');
+		}
+	}
+
+	// const a = uistuff.createNewNewsite("Big British Company");
+	// uistuff.addBiasLineToNewsite(a, "images/bias_far_left.png", "Far Left Leaning");
+	// uistuff.addArticleToNewsite(a, "https://jrgraphix.net/", "TestArticle", "Jan 1, 2000");
+
+	// let searchResults = await googlesearch.getGoogleSearchResults('bbc.com', 'obama');
+	// console.log(searchResults);
 }
 
 (async () => {
