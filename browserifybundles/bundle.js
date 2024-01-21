7beeda58c6961fcbb2c80f021658663e5eed6755
fjordimm <fjordimm@gmail.com>
2024-01-21 01:52:26 -0800

    getting url getting to work

--- browserifybundles/bundle.js
+++ browserifybundles/bundle.js
@@ -7,7 +7,21 @@ async function mainfunc()
 {
 	await mainNewsSites.addMainNewsSites();
 
-	console.log("Yourl: " + await googlesearching.generateGoogleSearchUrl('bbc.com', 'Bruh Stinky\n Yep'));
+	console.log("weeeeeeeeee");
+
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>
+	{
+		let u = tabs[0].url;
+		console.log("bronk: " + u);
+		onGottenPageUrl(u);
+	});
+
+	// console.log("Yourl: " + await googlesearching.generateGoogleSearchUrl('bbc.com', 'Bruh Stinky\n Yep'));
+}
+
+async function onGottenPageUrl(theUrl)
+{
+	console.log("weyEah: " + theUrl);
 }
 
 (async () => {
--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,28 @@
 
-const mainNewsSites = require('./scripts/ui/mainNewsSites')
+const mainNewsSites = require('./scripts/ui/mainNewsSites');
+const articlestuff = require('./scripts/webscraping/articlestuff');
 const googlesearching = require('./scripts/webscraping/googlesearching');
 
 async function mainfunc()
 {
 	await mainNewsSites.addMainNewsSites();
 
-	console.log("Yourl: " + await googlesearching.generateGoogleSearchUrl('bbc.com', 'Bruh Stinky\n Yep'));
+	console.log("weeeeeeeeee");
+	googlesearching.generateGoogleSearchUrl();
+
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>
+	{
+		let u = tabs[0].url;
+		console.log("bronk: " + u);
+		onGottenPageUrl(u);
+	});
+
+	// console.log("Yourl: " + await googlesearching.generateGoogleSearchUrl('bbc.com', 'Bruh Stinky\n Yep'));
+}
+
+async function onGottenPageUrl(theUrl)
+{
+	console.log("weyEah: " + theUrl);
 }
 
 (async () => {
