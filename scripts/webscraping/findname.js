aed8a2bfb82063c6529466f3b3dc3f2b9f4f2616
Navodit1603 <navoditmaheshwari@gmail.com>
2024-01-21 03:26:06 -0800

    changes if parameter is undefined

---
+++ scripts/webscraping/findname.js
@@ -0,0 +1,44 @@
+function FindName(url) {
+    if(url != "Undefined"){
+      let address = url;
+      let iter = 0;
+      let Pcount = 0;
+      let first = -1;
+      let second = -1;
+      let name = "";
+    
+      if (address.includes("eng.kavkaz-uzel.eu")) {
+        return "caucasianknot";
+      }
+    
+      let cumasscuzjason = address.indexOf("://");
+      cumasscuzjason = cumasscuzjason + 2;
+      //console.log(address.substring(cumasscuzjason + 1, cumasscuzjason + 4));
+      if (address.substring(cumasscuzjason + 1, cumasscuzjason + 4) == "www") {
+        while (Pcount < 2) {
+          if (address[iter] == ".") {
+            Pcount = Pcount + 1;
+            if (first > 0) {
+              second = iter;
+            } else {
+              first = iter;
+            }
+          }
+          iter = iter + 1;
+        }
+        name = address.substring(first + 1, second);
+      } else {
+        first = address.indexOf(".");
+        name = address.substring(cumasscuzjason + 1, first);
+      }
+    
+      console.log(name);
+      return name;
+      }
+    else{
+      return "Undefined";
+    }
+  }
+  
+  //console.log(FindName("https://reason.com/"));
+  
