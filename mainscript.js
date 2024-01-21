5442dae87acbe0ab2c7ead1af96dfca535b9302b
fjordimm <fjordimm@gmail.com>
2024-01-20 17:07:44 -0800

    set up project with npm, browserify, and esmify

---
+++ mainscript.js
@@ -0,0 +1,11 @@
+
+const exampleAddingSites = require('./scripts/ui/exampleAddingSites')
+
+function mainfunc()
+{
+	console.log('mainfunc() is working');
+
+	exampleAddingSites.thing();
+}
+
+mainfunc();
