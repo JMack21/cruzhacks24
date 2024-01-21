600e1e8c5729259d1880d57314735d5a4951f3a4
fjordimm <fjordimm@gmail.com>
2024-01-20 17:57:49 -0800

    testing stuff

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,9 @@
 
 const exampleAddingSites = require('./scripts/ui/exampleAddingSites')
 
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 function mainfunc()
 {
 	console.log('mainfunc() is working');
